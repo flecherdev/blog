51d056b77538d85433c068538eb929ad0edbd4d7
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2018-03-15 23:01:52 -0300

    se genera el modelo con Eloquent

---
+++ app/models/BlogPost.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\models;
+
+//Permite relacionar las tablas con el modelo de objetos
+use Illuminate\Database\Eloquent\Model;
+
+class BlogPost extends Model{
+
+    protected $table = 'blog-posts';
+    protected $fillable = ['title', 'content'];
+}
+
+?>
